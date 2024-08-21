11bca323d0953f56d80fad8647fe5ee93446fa4b
thedevelopersDeveloper <ashirwaddwi5510@gmail.com>
2024-08-21 12:49:52 +0000

    third commit

--- app/page.js
+++ app/page.js
@@ -4,25 +4,35 @@ const page = () => {
   const [title,setTitle] = useState("")
   const [desc,setDesc] = useState("")
   const [mainTask,setmainTask] = useState([])
+  let renderTask;
   const submitHandler = (e) => {
     e.preventDefault()
     setmainTask([...mainTask,{title,desc}])
-    
     setTitle("")
     setDesc("")
   }
-  let renderTask = <h2>No task available.</h2>;
+
+  const del = (i) => {
+    let copytask = [...mainTask]
+    copytask.splice(i,1)
+    setmainTask(copytask)
+  }
+
 
   renderTask = mainTask.map((elem,index)=>{
     return(
-      <li>
-      <h5>{elem.title}</h5>
-      <h6>{elem.desc}</h6>
+      <li key={index} className='mb-3 border-2 text-white px-2 py-2'>
+        <h5 className='text-underline'>{elem.title}</h5>
+        <h6>{elem.desc}</h6>
+        <button className='px-3 py-3 bg-red-600 rounded-lg' onClick={()=>{
+        del(index)
+        }}>Delete</button>
       </li>
     )
   })
+  
   return(
-    <div className='w-full h-screen bg-zinc-900 '>
+    <div className='w-full min-h-[100vh] bg-zinc-900 '>
       <div className='w-full h-[15vh] bg-zinc-800 flex items-center justify-center'>
         <h1 className='text-white text-6xl font-bold'>Todo App</h1>
       </div>
@@ -50,7 +60,7 @@ const page = () => {
             </form>
         </div>
         <div className='w-full h-[75vh] '>
-          <div className='w-full min-h-20 bg-red-900 px-2 py-2 mb-4'>
+          <div className='w-full min-h-20 px-2 py-2 mb-4'>
             <ul>
               {renderTask}
             </ul>
